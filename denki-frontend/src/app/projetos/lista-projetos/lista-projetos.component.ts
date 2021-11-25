29bc6acd57855b07421fe46a776dd55a78a5af11
Wellington <wgawebmail@gmail.com>
2021-11-24 23:18:40 -0300

    [FRONTEND] Funcionalidade de exclusão de projetos

--- denki-frontend/src/app/projetos/lista-projetos/lista-projetos.component.ts
+++ denki-frontend/src/app/projetos/lista-projetos/lista-projetos.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Projeto } from '../projeto';
-import { ProjetoService } from '../projeto.service';
+import { Component, OnInit } from '@angular/core'
+import { Projeto } from '../projeto'
+import { ProjetoService } from '../projeto.service'
 
 @Component({
   selector: 'denki-lista-projetos',
@@ -9,10 +9,20 @@ import { ProjetoService } from '../projeto.service';
 })
 export class ListaProjetosComponent implements OnInit {
 
-  projetos: Projeto[] = [];
+  projetos: Projeto[] = []
 
   constructor(private projetoService: ProjetoService) {
-    this.projetoService.listarProjetos().subscribe(projetos => this.projetos = projetos);
+    this.projetoService.listarProjetos().subscribe(projetos => this.projetos = projetos)
+  }
+
+  excluir(projeto: Projeto): void {
+    const resposta = confirm(`Deseja realmente excluir o projeto ${projeto.descricao}?`)
+    if (resposta) {
+      this.projetoService.excluir(projeto).subscribe(() => {
+        const node = document.getElementById(`${projeto.id}`)
+        node?.remove()
+      })
+    }
   }
 
   ngOnInit(): void { }
--- denki-frontend/src/app/projetos/projeto.service.ts
+++ denki-frontend/src/app/projetos/projeto.service.ts
@@ -1,22 +1,26 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Projeto } from './projeto';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { Projeto } from './projeto'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjetoService {
 
-  private url = 'denki-api/projetos';
-  private headers = new HttpHeaders().set('Content-Type', 'application/json');
+  private url = 'denki-api/projetos'
+  private headers = new HttpHeaders().set('Content-Type', 'application/json')
 
   listarProjetos(): Observable<Projeto[]> {
-    return this.http.get<Projeto[]>(this.url);
+    return this.http.get<Projeto[]>(this.url)
   }
 
   gravar(projeto: Projeto): Observable<Object> {
-    return this.http.post(this.url, projeto, { headers: this.headers });
+    return this.http.post(this.url, projeto, { headers: this.headers })
+  }
+
+  excluir(projeto: Projeto): Observable<Object> {
+    return this.http.delete(`${this.url}/${projeto.id}`)
   }
 
   constructor(private http: HttpClient) { }
